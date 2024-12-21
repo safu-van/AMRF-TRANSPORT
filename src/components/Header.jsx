0abb66849d0eb7650b70650fb7519b542bd94da9
safuvan <contactmesafwan@gmail.com>
2024-12-21 12:53:36 +0530

    favicon added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,7 +13,7 @@ const Header = () => {
       <div className="w-1/2 md:w-1/3 flex justify-start md:justify-center py-4 md:py-0 ">
         <img
           className="h-14 md:h-20 cursor-pointer"
-          src="amrf-logo.svg"
+          src="/amrf-logo.svg"
           alt="Logo"
         />
       </div>
@@ -21,17 +21,17 @@ const Header = () => {
       {/* Navigation Links */}
       <nav className="hidden md:flex md:justify-center md:w-1/3">
         <ul className="flex space-x-10">
-          <li className="hover:text-[#CC4242] cursor-pointer">Home</li>
-          <li className="hover:text-[#CC4242] cursor-pointer">About</li>
-          <li className="hover:text-[#CC4242] cursor-pointer">Service</li>
-          <li className="hover:text-[#CC4242] cursor-pointer">FAQs</li>
+          <li className="hover:text-[#CD4242] cursor-pointer">Home</li>
+          <li className="hover:text-[#CD4242] cursor-pointer">About</li>
+          <li className="hover:text-[#CD4242] cursor-pointer">Service</li>
+          <li className="hover:text-[#CD4242] cursor-pointer">FAQs</li>
         </ul>
       </nav>
 
       {/* Contact Us Button */}
       <div className="w-1/2 md:w-1/3 flex justify-end md:justify-center py-4 md:py-0">
-        <button className="relative bg-[#CC4242] w-24 md:w-32 h-10 rounded-md text-white overflow-hidden group">
-          <span className="absolute inset-0 bg-[#CC4242] transform translate-x-0 group-hover:translate-x-full transition-transform duration-500"></span>
+        <button className="relative bg-[#CD4242] w-24 md:w-32 h-10 rounded-md text-white overflow-hidden group">
+          <span className="absolute inset-0 bg-[#CD4242] transform translate-x-0 group-hover:translate-x-full transition-transform duration-500"></span>
           <span className="absolute inset-0 bg-[#005BAA] transform -translate-x-full group-hover:translate-x-0 transition-transform duration-500"></span>
           <span className="relative z-10">Contact Us</span>
         </button>
@@ -80,16 +80,16 @@ const Header = () => {
       {isMenuOpen && (
         <div className="absolute top-20 left-0 w-full bg-white shadow-md md:hidden">
           <ul className="flex flex-col px-6 space-y-4 py-4">
-            <li className="text-gray-700 hover:text-[#CC4242] cursor-pointer">
+            <li className="text-gray-700 hover:text-[#CD4242] cursor-pointer">
               Home
             </li>
-            <li className="text-gray-700 hover:text-[#CC4242] cursor-pointer">
+            <li className="text-gray-700 hover:text-[#CD4242] cursor-pointer">
               About
             </li>
-            <li className="text-gray-700 hover:text-[#CC4242] cursor-pointer">
+            <li className="text-gray-700 hover:text-[#CD4242] cursor-pointer">
               Services
             </li>
-            <li className="text-gray-700 hover:text-[#CC4242] cursor-pointer">
+            <li className="text-gray-700 hover:text-[#CD4242] cursor-pointer">
               FAQs
             </li>
           </ul>
